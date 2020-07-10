f12fd0e0f7f5023e977442f9a2fb80f592f5a701
CJW23 <cjw7242@gmail.com>
2020-07-10 13:34:40 +0900

    model fillable 작성

--- project/app/Model/Url.php
+++ project/app/Model/Url.php
@@ -4,7 +4,12 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/*
+ * 원본 url, 단축 url 저장할 모델
+ * */
 class Url extends Model
 {
-    //
+    protected $fillable = [
+        'user_id', 'original_url', 'short_url', 'query_string',
+    ];
 }
