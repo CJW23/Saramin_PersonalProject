c9848fc0c171b2e1cf49d5713c5f8f418c2b07e5
CJW23 <cjw7242@gmail.com>
2020-07-10 13:28:59 +0900

    table migrate 작성

---
+++ project/database/migrations/2020_07_10_034658_create_urls_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUrlsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('urls', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('original_url', 500);
+            $table->string('short_url', 5);
+            $table->string('query_string', 500)->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('url');
+    }
+}
