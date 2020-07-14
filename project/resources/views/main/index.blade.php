5272bcf16830994557dbe31242d596e0e3113ce1
CJW23 <cjw7242@gmail.com>
2020-07-14 16:25:45 +0900

    index페이지 UI개선

--- project/resources/views/main/index.blade.php
+++ project/resources/views/main/index.blade.php
@@ -1,9 +1,29 @@
-@extends('layouts.layout')
+@extends('layouts.app')
 
 @section('content')
+    <br><br>
     <div>
-        <input type="text" name="enter_url" id="enter_url">
-        <button onclick="requestCreateUrl()">Convert</button>
-        <a href="" id="shortUrl"></a>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-4">
+                    <img src="{{url('/images/saramin_bi_blue_english.png')}}" />
+                </div>
+            </div>
+            <br><br>
+            <div class="row justify-content-center">
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" name="enter_url" id="enter_url" placeholder="Enter your URL">
+                    <div class="input-group-append">
+                        <button onclick="requestCreateUrl()" class="btn btn-primary " type="button">Convert</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row justify-content-center">
+                <div class="col-4">
+                    <a href="" id="shortUrl"></a>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection()
